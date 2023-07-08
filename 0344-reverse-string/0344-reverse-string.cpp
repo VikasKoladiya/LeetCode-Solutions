f4ae029d580faa4f91b023f840b0a33b96f56d83
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2023-07-08 11:14:02 +0530

    Time: 17 ms (91.28%), Space: 23.1 MB (61.14%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        // int st=0;
-        // int e=s.size()-1;
-        // while(st<=e){
-        //     swap(s[st++],s[e--]);
-        // }
+        int st=0;
+        int e=s.size()-1;
+        while(st<=e){
+            swap(s[st++],s[e--]);
+        }
 
         // we can also use this one
         // this is very simple and easy this pribuild function of vector 
-        reverse(s.begin(),s.end());
+        // this one take more time compare to above case
+        // reverse(s.begin(),s.end());
     }
 };
