0a6f14b614211868fa115ecdc1390d2a0b8377f7
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2023-09-27 18:22:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (14.94%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> temp = heights;
-        sort(temp.begin(),temp.end());
-        int count =0;
-        for(int i=0;i<heights.size();i++){
-            if(temp[i]!=heights[i]){
-                count++;
-            }
+        vector<int>temp=heights;
+        sort(heights.begin(),heights.end());
+        int count=0;
+        for (int i=0; i<heights.size(); i++)
+        {
+            if (heights[i]!=temp[i]) count++;
         }
         return count;
     }
