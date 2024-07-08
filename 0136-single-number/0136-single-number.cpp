e7c0614af2f7e1d0632a0f8dcd49bb51d06246c9
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2024-07-08 15:03:30 +0530

    Time: 18 ms (35.08%), Space: 19.4 MB (29.76%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -4,25 +4,33 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int posi[30001] = {0};
-        int neg[30001] = {0};
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] < 0) {
-                neg[-nums[i]]++;
-            } else {
-                posi[nums[i]]++;
-            }
-        }
+        //1. Two Array Approach
+        // int posi[30001] = {0};
+        // int neg[30001] = {0};
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] < 0) {
+        //         neg[-nums[i]]++;
+        //     } else {
+        //         posi[nums[i]]++;
+        //     }
+        // }
         
-        for (int i = 0; i <= 30000; i++) {
-            if (neg[i] == 1) {
-                return i * (-1);
-            }
-            if (posi[i] == 1) {
-                return i;
-            }
-        }
+        // for (int i = 0; i <= 30000; i++) {
+        //     if (neg[i] == 1) {
+        //         return i * (-1);
+        //     }
+        //     if (posi[i] == 1) {
+        //         return i;
+        //     }
+        // }
         
-        return -1;
+        // return -1;
+
+        //2. XOR Method
+        int ans = 0;
+        for(int i=0;i<nums.size();i++){
+            ans^=nums[i];
+        }
+        return ans;
     }
 };
