da4a5d781dfe774acedfd9dc9fc80c90e4da1ebb
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2024-07-13 10:45:42 +0530

    Time: 27 ms (67.37%), Space: 13.2 MB (55.2%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char,int> m;
-        map<char,int> ind;
-        int ans = INT_MAX;
-        for(int i=0;i<s.length();i++){
-            m[s[i]]++;
-            ind[s[i]]=i;
-        }
-        for(auto p:m){
-            if(p.second==1){
-                if(ind[p.first]<ans){
-                    ans = ind[p.first];
-                }
-            }
-        }
-        if(ans == INT_MAX){
-            return -1;
-        }
-        return ans;
+        // map<char,int> m;
+        // map<char,int> ind;
+        // int ans = INT_MAX;
+        // for(int i=0;i<s.length();i++){
+        //     m[s[i]]++;
+        //     ind[s[i]]=i;
+        // }
+        // for(auto p:m){
+        //     if(p.second==1){
+        //         if(ind[p.first]<ans){
+        //             ans = ind[p.first];
+        //         }
+        //     }
+        // }
+        // if(ans == INT_MAX){
+        //     return -1;
+        // }
+        // return ans;
+        vector<int> mp(26, 0);
+        for(auto i : s) mp[i-'a']++;
+        for(int i  = 0;i < s.length();i++)
+            if(mp[s[i] - 'a'] == 1) return i;
+        
+        return -1;
     }
 };
