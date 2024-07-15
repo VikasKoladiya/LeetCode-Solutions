b291f659499ead976f077940d62720c68e54b266
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2024-07-15 20:58:33 +0530

    Time: 0 ms (100%), Space: 10.7 MB (5.85%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,21 +1,52 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-        vector<string> ans;
-        for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
-                ans.push_back("FizzBuzz");
-            }
-            else if(i%3==0){
-                ans.push_back("Fizz");
-            }
-            else if(i%5==0){
-                ans.push_back("Buzz");
-            }
-            else{
-                ans.push_back(to_string(i));
-            }
+        // 1.
+        // vector<string> ans;
+        // for(int i=1;i<=n;i++){
+        //     if(i%3==0 && i%5==0){
+        //         ans.push_back("FizzBuzz");
+        //     }
+        //     else if(i%3==0){
+        //         ans.push_back("Fizz");
+        //     }
+        //     else if(i%5==0){
+        //         ans.push_back("Buzz");
+        //     }
+        //     else{
+        //         ans.push_back(to_string(i));
+        //     }
+        // }
+        // return ans;
+
+        // 2.
+        if( n == 1 ){
+            return {"1"};
         }
-        return ans;
+        if( n == 2 ){
+            return {"1","2"};
+        }
+        vector<string> ans(n);
+        ans[0] = "1";
+        ans[1] = "2";
+        int i = 3;
+        while( i <= n ){
+            int chia3 = i%3;
+            int chia5 = i%5;
+            if( chia3 == 0 && chia5 == 0 ){
+                ans[i-1] = "FizzBuzz";
+            }
+            else if( chia3 == 0 && chia5 != 0 ){
+                ans[i-1] = "Fizz";
+            }
+            else if( chia3 != 0 && chia5 == 0 ){
+                ans[i-1] = "Buzz";
+            }
+            else {
+                ans[i-1] = to_string(i);
+            }
+            i++;
+        }
+        return ans; 
     }
 };
