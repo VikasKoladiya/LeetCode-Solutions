bb753a390a9f31c1a50414bc04722ea8840a8fb0
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2024-07-22 15:15:26 +0530

    Time: 36 ms (31.48%), Space: 27.6 MB (27.83%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         int n = heights.size();
+        vector<string> v;
+        map<int,string> m;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(heights[i]<heights[j]){
-                    swap(heights[i],heights[j]);
-                    swap(names[i],names[j]);
-                }
-            }
+            m[heights[i]]=names[i];
         }
-        return names;
+        for(auto p:m){
+            v.push_back(p.second);
+        }
+        reverse(v.begin(),v.end());
+        return v;
     }
 };
