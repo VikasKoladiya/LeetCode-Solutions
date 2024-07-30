7d740de735328529ffaf8b53f5b22e36338cebd5
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2024-07-30 14:09:29 +0530

    Time: 7 ms (97.13%), Space: 10.3 MB (68.08%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int> ransom;
-        map<char,int> magazie;
-        for(int i=0;i<ransomNote.length();i++){
-            ransom[ransomNote[i]]++;
+        vector<int> v(26);
+        for(auto ch : magazine){
+            v[ch-'a']++;
         }
-        for(int i=0;i<magazine.length();i++){
-            magazie[magazine[i]]++;
-        }
-        for(auto p:ransom){
-            if(magazie[p.first]<p.second){
+        for(auto ch : ransomNote){
+            if(v[ch-'a']-- <= 0){
                 return false;
             }
         }
