703e628e0916343a8ba75f2ee1dd501e5cfa126d
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2024-12-27 23:46:52 +0530

    Time: 91 ms (18.59%), Space: 73.1 MB (55.76%) - LeetHub

--- 2831-number-of-beautiful-pairs/2831-number-of-beautiful-pairs.cpp
+++ 2831-number-of-beautiful-pairs/2831-number-of-beautiful-pairs.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
+    int gcd(int a,int b){
+        return b==0 ? a: gcd(b,a%b);
+    }
     int countBeautifulPairs(vector<int>& nums) {
         int ans = 0;
-        for(int i=0;i<nums.size()-1;i++){
-            int num = nums[i];
-            if(num>9 && num<100){
-                num/=10;
-            }
-            else if(num>99 && num<1000){
-                num/=100;
-            }
-            else if(num>999 && num<10000){
-                num/=1000;
-            }
+        for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
-                int a = min(num,nums[j]%10);
-                int b = max(num,nums[j]%10);
-                while(b!=0){
-                    int temp = b;
-                    b = a%b;
-                    a = temp;
-                }
-                if(a==1){
+                if(gcd(to_string(nums[i])[0]-'0',nums[j]%10)==1){
                     ans++;
                 }
             }
