539fd6617cd5fa7b76090fdf3ff20d3cf4293797
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2025-03-17 18:00:38 +0530

    Time: 171 ms (10.4%), Space: 124 MB (19.85%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        set<int> s;
+        int n = candyType.size();
+        for(int i=0;i<n;i++){
+            s.insert(candyType[i]);
+        }
+        if(s.size()>n/2){
+            return n/2;
+        }
+        return s.size();
+    }
+};
