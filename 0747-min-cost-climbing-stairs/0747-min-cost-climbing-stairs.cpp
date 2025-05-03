59a3bf285eef86c38abee4cf3d1f7ffe445fc56d
Vikas Koladiya <114695020+VikasKoladiya@users.noreply.github.com>
2025-05-03 16:26:35 +0530

    Time: 0 ms (100%), Space: 18 MB (6%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // top-down approach
     int solve(vector<int> cost,int n,vector<int> &dp){
         // Base case
         if(n==0){
@@ -19,11 +20,26 @@ public:
         return dp[n];
     }
 
+
+    int solve2(vector<int> cost, int n){
+        vector<int> dp(n+1);
+         dp[0]=cost[0];
+         dp[1]=cost[1];
+
+         for(int i=2;i<n;i++){
+            dp[i]=cost[i] + min(dp[i-1],dp[i-2]);
+         }
+
+         return min(dp[n-1],dp[n-2]);
+    }
+
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp(n+1,-1);
 
-        int ans = min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+        // int ans = min(solve(cost,n-1,dp),solve(cost,n-2,dp));
+
+        int ans = solve2(cost,n);
         return ans;
     }
 };
